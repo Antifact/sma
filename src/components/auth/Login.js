7e0def142aa4a93fb0fa040df334bd0b03bdf6cd
Lily Lloyd-Kassulke <lilyrose@Lilys-MacBook-Pro.local>
2023-03-14 04:23:22 +1000

    working login and register

--- src/components/auth/Login.js
+++ src/components/auth/Login.js
@@ -2,27 +2,31 @@ import React, { useState } from "react";
 import { collection, where, query, getDocs } from "firebase/firestore";
 import bcrypt from "bcryptjs";
 import { db } from '../../utils/firebase';
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+import { useNavigate } from "react-router-dom";
 
 
 export const Login = () => {
-  const [email, setEmail] = useState("");
+  const [username, setUsername] = useState("");
   const [password, setPassword] = useState("");
+  const navigate = useNavigate();
 
   const handleLogin = async (e) => {
     e.preventDefault();
     const usersRef = collection(db, "users");
-    const q = query(usersRef, where("email", "==", email));
+    const q = query(usersRef, where("username", "==", username));
     const querySnapshot = await getDocs(q);
     if (querySnapshot.empty) {
-      console.log("User not found");
-      return;
+        console.log("User not found");
+        return;
     }
     const userDoc = querySnapshot.docs[0];
     const userData = userDoc.data();
     const passwordMatch = await bcrypt.compare(password, userData.password);
     if (passwordMatch) {
       console.log("Login successful!");
-      // Perform any additional actions, such as updating the Redux store or redirecting to a new page.
+        navigate("/");
     } else {
       console.log("Invalid password");
     }
@@ -30,8 +34,8 @@ export const Login = () => {
 
   return (
     <form onSubmit={handleLogin}>
-      <label htmlFor="email">Email</label>
-      <input type="email" id="email" value={email} onChange={(e) => setEmail(e.target.value)} />
+      <label htmlFor="username">Username</label>
+      <input type="username" id="username" value={username} onChange={(e) => setUsername(e.target.value)} />
 
       <label htmlFor="password">Password</label>
       <input type="password" id="password" value={password} onChange={(e) => setPassword(e.target.value)} />
--- src/components/auth/Register.js
+++ src/components/auth/Register.js
@@ -1,96 +1,110 @@
 import React, { useState } from "react";
-import { collection, addDoc } from "firebase/firestore";
+import { collection, addDoc, where, query, getDocs, getDoc } from "firebase/firestore";
 import bcrypt from "bcryptjs";
 import { db } from '../../utils/firebase';
+import { Firestore } from "firebase/firestore";
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 const Register = () => {
-  const [username, setUsername] = useState("");
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
-  const [confirmPassword, setConfirmPassword] = useState("");
+    const [username, setUsername] = useState("");
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+    const [confirmPassword, setConfirmPassword] = useState("");
+    const [error, setError] = useState(null);
 
-  const handleSubmit = async (event) => {
-    event.preventDefault();
+
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+
+        // console.log(db);
 
     if (password !== confirmPassword) {
-      alert("Passwords do not match");
-      return;
+        alert("Passwords do not match");
+        return;
     }
 
     try {
-      const usersRef = collection(db, "users");
+        const usersRef = collection(db, "users");
+        const q = query(usersRef, where("username", "==", username || "email", "==", email));
+        const querySnapshot = await getDocs(q);
 
-      const snapshot = await usersRef.get();
-      const userId = snapshot.docs.length + 1;
+        if (!querySnapshot.empty) {
+            console.log("Username or email already exists!");
+            return;
+        }
 
-      const hashedPassword = await bcrypt.hash(password, 10);
+        const userId = querySnapshot.docs.length + 1;
 
-      const newUser = {
-        id: userId,
-        username,
-        email,
-        password: hashedPassword,
-      };
+        const hashedPassword = await bcrypt.hash(password, 10);
 
-      await addDoc(usersRef, newUser);
+        const newUser = {
+            id: userId,
+            username,
+            email,
+            password: hashedPassword,
+        };
 
-      setUsername("");
-      setEmail("");
-      setPassword("");
-      setConfirmPassword("");
+        await addDoc(usersRef, newUser);
 
-      alert("Registration successful!");
+        setUsername("");
+        setEmail("");
+        setPassword("");
+        setConfirmPassword("");
+
+        alert("Registration successful!");
+
     } catch (error) {
-      console.error(error);
-      alert("Registration failed");
-    }
-  };
+            console.error(error);
+            alert("Registration failed");
+        }
+    };
 
-  return (
-    <form onSubmit={handleSubmit}>
-      <label>
-        Username:
-        <input
-          type="text"
-          value={username}
-          onChange={(event) => setUsername(event.target.value)}
-          required
-        />
-      </label>
-      <br />
-      <label>
-        Email:
-        <input
-          type="email"
-          value={email}
-          onChange={(event) => setEmail(event.target.value)}
-          required
-        />
-      </label>
-      <br />
-      <label>
-        Password:
-        <input
-          type="password"
-          value={password}
-          onChange={(event) => setPassword(event.target.value)}
-          required
-        />
-      </label>
-      <br />
-      <label>
-        Confirm Password:
-        <input
-          type="password"
-          value={confirmPassword}
-          onChange={(event) => setConfirmPassword(event.target.value)}
-          required
-        />
-      </label>
-      <br />
-      <button type="submit">Register</button>
-    </form>
-  );
+    return (
+        <form onSubmit={handleSubmit}>
+        <label>
+            Username:
+            <input
+            type="text"
+            value={username}
+            onChange={(e) => setUsername(e.target.value)}
+            required
+            />
+        </label>
+        <br />
+        <label>
+            Email:
+            <input
+            type="email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+            required
+            />
+        </label>
+        <br />
+        <label>
+            Password:
+            <input
+            type="password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+            required
+            />
+        </label>
+        <br />
+        <label>
+            Confirm Password:
+            <input
+            type="password"
+            value={confirmPassword}
+            onChange={(e) => setConfirmPassword(e.target.value)}
+            required
+            />
+        </label>
+        <br />
+        <button type="submit">Register</button>
+        </form>
+    );
 };
 
 export default Register;
